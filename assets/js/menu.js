a07eb83ebe176d43a5e4cf2b4d0c5b03a191ae00
Devan <huapayadevan@gmail.com>
2016-11-18 13:49:05 -0600

    Update 2016-11-18T19:49:02.382Z

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,89 +1,52 @@
-function gei (id) {
-    return document.getElementById(id);
+var nav = window.nav || {};
+
+nav.TopNav= function() {
+  this.container = document.querySelector("[data-top-nav='container']");
+  this.icon = document.querySelector("[data-top-nav='icon']");
+  this.text = document.querySelector("[data-top-nav='text']");
+  this.elements = [this.container, this.icon, this.text];
+  this.position = 0;
 }
-function qsa (qs) {
-    return document.querySelectorAll(qs);
+
+nav.TopNav.prototype.init = function() {
+  console.log('init');
+    this.initScroll();
 }
 
-var m, menu = {
-    elms: {
-        button: gei('menu-button'),
-        container: gei('menu-container'),
-        bar: gei('menu-bar'),
-        navButtons: qsa('.menu__button'),
-        isOpen: function() {
-            return this.button.classList.contains('open');
-        }
-    },
-    addEvents: function() {
-        var ths = this;
-        m.button.addEventListener('click', function() {
-            ths.toggle();
-        });
-        document.onkeydown = function(evt) {
-            evt = evt || window.event;
-            var isEscape = false;
-            if ("key" in evt) {
-                isEscape = (evt.key == "Escape" || evt.key == "Esc");
-            } else {
-                isEscape = (evt.keyCode == 27);
-            }
+nav.TopNav.prototype.hide = function() {
+  this.container.classList.add('hide');
+}
 
-            if (isEscape) {
-                ths.close();
-            }
-        };
-    },
-    initScroll: function() {
-        var ths = this
-            last = 0,
-            current = 0;
+nav.TopNav.prototype.unHide = function() {
+  this.container.classList.remove('hide');
+}
 
-        window.setInterval(function() {
-            current = document.body.scrollTop;
+nav.TopNav.prototype.initScroll = function() {
+    var ths = this;
 
-            if (current === 0){
-                m.navButtons[0].classList.add('top');
-                m.navButtons[1].classList.add('top');
-            } else {
-                m.navButtons[0].classList.remove('top');
-                m.navButtons[1].classList.remove('top');
-            }
-            if (current === last || m.isOpen()){
-                return;
-            }
-            else if(current > last ){
-                m.bar.classList.add('close');
-                console.log("close");
-            } else {
-                m.bar.classList.remove('close');
-                console.log("open");
-            }
-            last = current;
-        }, 250);
-    },
-    init: function() {
-        m = this.elms;
-        this.addEvents();
-        this.initScroll();
-    },
-    open: function () {
-        m.button.classList.add('open');
-        m.container.classList.add('open');
-    },
-    close: function () {
-        m.button.classList.remove('open');
-        m.container.classList.remove('open');
-    },
-    toggle: function() {
-        var ths = this;
+  window.setInterval(function() {
+    var pos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
-        if(m.isOpen()) {
-             ths.close();
-        } else {
-            ths.open();
-        }
+    if (pos < 1){
+      ths.container.classList.add('onTop');
+    } else {
+      ths.container.classList.remove('onTop');
     }
+    console.log(ths.position);
+    if (pos < ths.position) {
+      ths.unHide();
+    } else if (pos > ths.position) {
+      ths.hide();
+    }
+
+    ths.position = pos;
+  }, 250);
 }
 
-menu.init();
+nav.TopNav.prototype.onTop = function() {
+    console.log(pos);
+    return pos < 1
+}
+
+nav.instance = new nav.TopNav;
+nav.instance.init();
